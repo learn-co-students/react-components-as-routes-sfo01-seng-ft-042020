dc8d1e692f7892ce100c3e2f44c373feea068a22
tharveybrown <tharveybrown@gmail.com>
2020-06-29 11:54:23 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -2,74 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom";
 // Step 1. Import react-router functions
 import { BrowserRouter as Router, Route, NavLink } from "react-router-dom";
+import NavBar from "./NavBar";
+import Login from "./Login";
+import Home from "./Home";
+import About from "./About";
 
-const link = {
-  width: "100px",
-  padding: "12px",
-  margin: "0 6px 6px",
-  background: "blue",
-  textDecoration: "none",
-  color: "white",
-};
-
-const NavBar = () => {
-  return (
-    <div>
-      <NavLink
-        to="/"
-        exact
-        style={link}
-        activeStyle={{ background: "darkblue" }}
-      >
-        Home
-      </NavLink>
-      <NavLink
-        to="/about"
-        exact
-        style={link}
-        activeStyle={{ background: "darkblue" }}
-      >
-        About
-      </NavLink>
-      <NavLink
-        to="/login"
-        exact
-        style={link}
-        activeStyle={{ background: "darkblue" }}
-      >
-        Login
-      </NavLink>
-    </div>
-  );
-};
-
-const Home = () => <h1>Home!</h1>;
-
-const About = () => {
-  return (
-    <div>
-      <h1>This is my about component!</h1>
-    </div>
-  );
-};
-
-const Login = () => {
-  return (
-    <div>
-      <form>
-        <div>
-          <input type="text" name="username" placeholder="Username" />
-          <label htmlFor="username">Username</label>
-        </div>
-        <div>
-          <input type="password" name="password" placeholder="Password" />
-          <label htmlFor="password">Password</label>
-        </div>
-        <input type="submit" value="Login" />
-      </form>
-    </div>
-  );
-};
 // Step 2. Changed to have router coordinate what is displayed
 ReactDOM.render(
   <Router>
